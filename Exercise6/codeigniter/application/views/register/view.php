45811a0770d902096c0a02edbac99d653d4c6137
thomllantos@gmail.com <thomllantos@gmail.com>
2016-12-21 22:16:18 +0800

    view.php

---
+++ Exercise6/codeigniter/application/views/register/view.php
@@ -0,0 +1,20 @@
+<center>
+<table  cellspacing="7" style="margin-top: -20px">
+	<tr>
+		<th>Name</th>
+		<th>Nickname</th>
+		<th>Email</th>
+		<th>Phone Number</th>
+		<th>Address</th>
+		<th>Gender</th>
+		<th>Comment</th>
+	</tr>
+	<tr>
+        <td><?php echo $user_item['name']; ?></td>
+        <td><?php echo $user_item['nickname']; ?></td>
+        <td><?php echo $user_item['email']; ?></td>
+        <td><?php echo $user_item['phone']; ?></td>
+        <td><?php echo $user_item['homead']; ?></td>
+        <td><?php echo $user_item['gender']; ?></td>
+        <td><?php echo $user_item['comment']; ?></td>
+    </tr>
